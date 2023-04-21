3ae8692bc923acd3c745030504fc44a46cc1b88f
Frances Leigh <fleighbaay@gmail.com>
2023-04-21 13:49:30 +0800

    Use dictionary

---
+++ Vowel Decryption.py
@@ -0,0 +1,8 @@
+#Use dictionary to define the subtitutes
+
+char_substitute = {'*': 'a', '&': 'e', '#': 'i', '+': 'o', '!': 'u'}
+
+#Prompt the user to input a text
+#Check if a character from the dictionary is present in the encrypted_text
+#If character is present, it will retrieve the value and will be appended to decrypted text
+#Print the output
