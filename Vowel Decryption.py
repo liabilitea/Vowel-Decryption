9dca912919404561281473b97f89a94084003147
Frances Leigh <fleighbaay@gmail.com>
2023-04-21 14:00:46 +0800

    Input text

--- Vowel Decryption.py
+++ Vowel Decryption.py
@@ -3,6 +3,10 @@
 char_substitute = {'*': 'a', '&': 'e', '#': 'i', '+': 'o', '!': 'u'}
 
 #Prompt the user to input a text
+encrypted_text = input("\033[3mEnter an encrypted text to decrypt:\033[0m ")
+
+decrypted_text = ""
+
 #Check if a character from the dictionary is present in the encrypted_text
 #If character is present, it will retrieve the value and will be appended to decrypted text
 #Print the output
